c539f51412a9281ef420ed31ea80d0ee0bfd24f7
Kevan Ahlquist <ahlqu039@umn.edu>
2014-11-17 23:03:17 -0600

    Switch app to fullscreen.

--- app/src/main/java/com/breaktime/BackToWorkActivity.java
+++ app/src/main/java/com/breaktime/BackToWorkActivity.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -25,26 +23,6 @@ public class BackToWorkActivity extends Activity {
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         long[] pattern = {100L, 200L,100L, 200L,100L, 200L};
         vibrator.vibrate(pattern, -1);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.back_to_work, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
--- app/src/main/java/com/breaktime/StudyTimerActivity.java
+++ app/src/main/java/com/breaktime/StudyTimerActivity.java
@@ -16,8 +16,6 @@ import android.os.PowerManager;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -69,26 +67,6 @@ public class StudyTimerActivity extends Activity implements SensorEventListener 
         Log.v("StudyTimerActivity", "millis:" + remainingMillis);
         long seconds = remainingMillis / 1000;
         timerTextView.setText(String.format("%d", seconds));
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.study_timer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
